7eb0bf9d3d36ebc545318f31e98425965947e3a5
ashajyothi06 <120666428+ashajyothi06@users.noreply.github.com>
2026-08-12 21:17:53 +0530

    changed url

--- vite.config.ts
+++ vite.config.ts
@@ -3,15 +3,16 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+
   server: {
     host: "0.0.0.0",
     port: 5173,
-   proxy: {
-  "/api": {
-    target: "https://gyaini.com:6880",
-    changeOrigin: true,
-    secure: false,
+
+    proxy: {
+      "/api": {
+        target: "http://127.0.0.1:1880",
+        changeOrigin: true,
+      },
+    },
   },
-},
-  }
 });
